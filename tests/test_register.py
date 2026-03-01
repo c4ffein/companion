1553ae2476a960cb05f429a7feaff9594daa84b8
c4ffein <c4ffein@gmail.com>
2026-03-01 13:48:00 +0100

    test_register.py: added future list of tests

---
+++ tests/test_register.py
@@ -0,0 +1,10 @@
+# TODO a test where a client tries to register on a non-running server
+# TODO a test where a client tries to register on a server without the correct config
+# TODO a test where a client tries to register on a server with the correct config but no admin rights
+# TODO a test where a client tries to register on a server without the correct config and no admin rights
+# TODO a test where a client tries to register on a server with the correct config and admin rights
+#  - new user is created on the server
+#  - new user is shown in the cli
+#  - locks etc worked
+# TODO same test but with a provided id
+# TODO same test but with a provided id + password
